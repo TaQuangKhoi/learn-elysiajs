e907303624cd4588b01bb410ddd760334bed05b7
Tạ Quang Khôi <khoi0941@gmail.com>
2025-06-07 00:50:03 +0700

    Refactor index.ts to simplify app initialization
    Reorganized and cleaned up main app setup. Replaced inline route and service definitions with imported `appRoutes`. Streamlined error handling with `onError` and removed unused classes and methods.

--- src/index.ts
+++ src/index.ts
@@ -1,72 +1,14 @@
-import {Elysia} from "elysia";
-import {t} from 'elysia'
-import {customBody} from "./customerBody";
-import {swagger} from '@elysiajs/swagger'
-
-const store = new Elysia()
-  .state({visitor: 0})
-
-const router = new Elysia()
-  .use(store)
-  .get('/increase', ({store}) => store.visitor++)
-
-abstract class Controller {
-  static greet({name}: { name: string }) {
-    return 'hello ' + name
-  }
-}
-
-abstract class Service {
-  static fibo(number: number): number {
-    if (number < 2)
-      return number
-
-    return Service.fibo(number - 1) + Service.fibo(number - 2)
-  }
-}
-
-const AuthService = new Elysia({name: 'Service.Auth'})
-  .derive({as: 'scoped'}, ({cookie: {session}}) => ({
-    // This is equivalent to dependency injection
-    Auth: {
-      user: session.value
-    }
-  }))
-  .macro(({onBeforeHandle}) => ({
-    // This is declaring a service method
-    isSignIn(value: boolean) {
-      onBeforeHandle(({Auth, status}) => {
-        if (!Auth?.user || !Auth.user) return status(401)
-      })
-    }
-  }))
-
-const UserController = new Elysia()
-  .use(AuthService)
-  .get('/profile', ({Auth: {user}}) => user, {
-    isSignIn: true
-  })
+import { Elysia } from "elysia";
+import { swagger } from '@elysiajs/swagger';
+import { appRoutes } from './presentation/routes';
 
 const app = new Elysia()
-  .use(router)
-  .use(swagger())
-  .get("/", () => "Hello Elysia")
-  .post('/test', ({body}) => Controller.greet(body), {
-    body: t.Object({
-      name: t.String()
-    })
+  .use(swagger()) // Tích hợp Swagger để test API
+  .use(appRoutes) // Sử dụng các routes đã định nghĩa
+  .onError(({ code, error }) => {
+    console.error(`Error: ${error.toString()}`);
+    return new Response(error.toString(), { status: 500 });
   })
-  .get('/fibo', ({body}) => {
-    return Service.fibo(body)
-  }, {
-    body: t.Numeric()
-  })
-  .post('/login', ({body}) => {
-    return body
-  }, {
-    body: customBody
-  })
-
   .listen(3000);
 
 console.log(
