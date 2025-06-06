6e90075ed2d838015177a836ba6a6fb292902803
Tạ Quang Khôi <khoi0941@gmail.com>
2025-06-06 23:29:05 +0700

    [Message] Add customBody validation schema with Elysia
    Introduced a `customBody` schema using Elysia's type system for username and password validation. The schema also includes an optional type export for inferred type usage.

---
+++ src/customerBody.ts
@@ -0,0 +1,15 @@
+// ✅ Do
+import { Elysia, t } from 'elysia'
+
+const customBody = t.Object({
+	username: t.String(),
+	password: t.String()
+})
+
+// Optional if you want to get the type of the model
+// Usually if we didn't use the type, as it's already inferred by Elysia
+type CustomBody = typeof customBody.static
+
+
+
+export { customBody }
